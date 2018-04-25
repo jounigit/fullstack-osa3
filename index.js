ac39f20fc5ceb1c4791914ae13278d78ab82a0a8
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-04-25 11:31:36 +0300

    3.4 puhelinluettelon backend osa 4

--- index.js
+++ index.js
@@ -45,15 +45,22 @@ let persons = [
   })
 
   app.get('/api/persons/:id', (req, response) => {
-  const id = Number(req.params.id)
-  const person = persons.find(person => person.id === id)
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
 
-  if ( person ) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})
+    if ( person ) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+  })
 
   const PORT = process.env.PORT || 3001
   app.listen(PORT, () => {
