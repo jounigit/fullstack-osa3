88d5d12103d03dfb6b78752b88f77457d8391477
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-05-15 11:10:01 +0300

    models kansio

---
+++ models/person.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://fullstack:fullstackja@ds119090.mlab.com:19090/persons-ja'
+// const url = 'mongodb://<user>:<pass>@ds119090.mlab.com:19090/persons-ja'
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+module.exports = Person
