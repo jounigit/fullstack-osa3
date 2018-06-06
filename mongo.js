833c3979a9de3c3aecfaf3a9656657b45819afa8
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-06-06 14:57:34 +0300

    Remove the now ignored directory node_modules

--- mongo.js
+++ mongo.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://<user>:<pass>@ds119090.mlab.com:19090/persons-ja'
+if ( process.env.NODE_ENV !== 'production' ) {
+  require('dotenv').config()
+}
+
+const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
 
