050b8cb7b7d2b6b8c9cc42adb09206fa62df9e42
Jouni Airaksinen <jouni.airaksinen@dnainternet.net>
2018-06-06 13:21:40 +0300

    Remove the now ignored directory node_modules

--- models/person.js
+++ models/person.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
 
-const url = 'mongodb://fullstack:fullstackja@ds119090.mlab.com:19090/persons-ja'
-// const url = 'mongodb://<user>:<pass>@ds119090.mlab.com:19090/persons-ja'
+const url = ''
 
 mongoose.connect(url)
 
